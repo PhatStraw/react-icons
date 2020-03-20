f9180c856d53401a44b67a3fc244a6e86420d3ff
kevinsims1 <kevinsims1@outlook.com>
2020-03-19 23:28:15 -0700

    init

--- src/components/bare.js
+++ src/components/bare.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-//css
-import "../css/bare.css"
+import { FaTwitter} from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const Bare = () => {
-  return <div>Welcome to bare react!</div>;
+  return (
+    <IconContext.Provider value={{ style: { fontSize: '30px', color: "rgb(0, 123, 255)" } }}>
+      <div>
+        <FaTwitter />
+      </div>
+    </IconContext.Provider>
+  );
 };
 
 export default Bare;
